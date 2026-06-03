6bc88eb7759a11bfdcbbc791215815ab97ed6cbf
Nithish <nithish221b@gmail.com>
2026-06-03 18:44:33 +0530

    Agent for giving complement for a name

---
+++ Agents/Agent2_Complement.py
@@ -0,0 +1,23 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph
+
+class ComplementState(TypedDict):
+    name : str
+
+def complementNode(state : ComplementState) -> ComplementState:
+    """Simple function to complement the name add to the state."""
+    state["name"] = "Hi " + state["name"] + ", you look great today!!. You look stunning!!"
+    return state
+
+graph = StateGraph(ComplementState)
+graph.add_node("complement", complementNode)
+graph.set_entry_point("complement")
+graph.set_finish_point("complement")
+
+app = graph.compile()
+
+
+
+result = app.invoke({"name": "Snigdha"})
+print(result["name"])
+
