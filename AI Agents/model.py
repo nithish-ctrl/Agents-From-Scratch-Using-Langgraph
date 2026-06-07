c76ceed497b661ec3f04dbff8cccbb54d72ed058
Nithish <nithish221b@gmail.com>
2026-06-07 21:49:17 +0530

    model file

---
+++ AI Agents/model.py
@@ -0,0 +1,20 @@
+from langchain_community.chat_models import ChatLlamaCpp
+
+def load_model() : 
+    model_path = r"c:\Users\Nithish\Downloads\qwen2.5-3b-instruct-q5_0.gguf"
+    llm = ChatLlamaCpp(
+        model_path = model_path,
+        temperature = 0.1,
+        streaming = False, 
+        max_tokens = 128,
+        n_ctx = 2048,
+        n_batch = 512,
+        top_p = 0.9,
+        model_kwargs={
+            "device" : "cuda",
+            "chat_format" : "chatml",
+            "flash_attn" : True
+        },
+        verbose=False
+    )
+    return llm
