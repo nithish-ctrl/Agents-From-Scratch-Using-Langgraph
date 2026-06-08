4655346bece9e09dc36411164b0c082d4e5c5d3a
Nithish <nithish221b@gmail.com>
2026-06-08 22:04:27 +0530

    Annotated from Typing

--- Basics/Annotated.py
+++ Basics/Annotated.py
@@ -2,4 +2,4 @@ from typing import Annotated
 
 email = Annotated[str, "It has to be a valid email address"]
 # The string has after the type gets added to the meta data
-print(email.__metadata__)
+print(email.__metadata__) # type: ignore
